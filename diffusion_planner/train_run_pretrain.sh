a8d2d0683925f8659e4760a8a05c3a0920e1b936
Y.Hisaki <yhisaki31@gmail.com>
2026-06-08 15:39:34 +0900

    remove validation and save model until epoch
    Signed-off-by: Y.Hisaki <yhisaki31@gmail.com>

---
+++ diffusion_planner/train_run_pretrain.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -ux
+exp_name=${1}
+TRAIN_SET_LIST=${2:-/mnt/nvme/dataset/basic_dataset/path_list_train.json}
+VALID_SET_LIST=${3:-/mnt/nvme/dataset/basic_dataset/path_list_valid.json}
+SFT_SET_LIST=${4:-/mnt/nvme/dataset/basic_dataset/path_list_valid_sft.json}
+DEBUG=${5:-False}
+
+# to convert full paths
+TRAIN_SET_LIST=$(readlink -f $TRAIN_SET_LIST)
+VALID_SET_LIST=$(readlink -f $VALID_SET_LIST)
+SFT_SET_LIST=$(readlink -f $SFT_SET_LIST)
+
+cd $(dirname $0)
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+export NCCL_NVLS_ENABLE=0
+export NCCL_P2P_DISABLE=0
+export NCCL_IB_DISABLE=1
+export NCCL_SOCKET_IFNAME=lo
+export NCCL_DEBUG=INFO
+
+export DIST_INIT_FILE=/tmp/tmp_dist_init_$$
+rm -f ${DIST_INIT_FILE}
+
+SAVE_DIR="./training_result"
+TIME=$(date +%Y%m%d-%H%M%S)
+SAVE_PATH="${SAVE_DIR}/${TIME}_${exp_name}"
+
+mkdir -p ${SAVE_PATH}
+
+# Save git info
+git show -s > ${SAVE_PATH}/git_show.txt
+git diff > ${SAVE_PATH}/git_diff.txt
+
+# pretraining
+LAUNCHER=(python3)
+if [ "${DEBUG}" = "True" ]; then
+    LAUNCHER=(python3 -m debugpy --listen 5678)
+fi
+
+"${LAUNCHER[@]}" -m torch.distributed.run --nnodes 1 --nproc-per-node 8 --standalone train_predictor.py \
+--exp_name ${exp_name} \
+--train_set_list $TRAIN_SET_LIST \
+--valid_set_list $VALID_SET_LIST \
+--use_wandb False \
+--diffusion_model_type "x_start" \
+--save_dir ${SAVE_PATH} \
+--train_epochs 80 \
+--save_utd 10 \
+2>&1 | tee ${SAVE_PATH}/train_log.txt
